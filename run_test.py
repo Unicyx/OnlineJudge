113d5ba06f8325f42ef074bdd6679b13c016beed
Unicyx <2428250383@qq.com>
2023-11-05 19:52:59 +0800

    一个尝试

--- run_test.py
+++ run_test.py
@@ -25,3 +25,5 @@ ret = os.system('coverage run --include="$PWD/*" manage.py test {module} --setti
 
 if not ret and is_coverage:
     os.system("coverage html && open htmlcov/index.html")
+
+##wtaudoing
